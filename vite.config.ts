21ddbae4fbe35045a8d3a45eca93feec2957c2e7
aliNajib <alinajibhegazy4@gmail.com>
2025-03-09 15:17:36 +0200

    add firebase file

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,14 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), 
-      "@src": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
       "@components": path.resolve(__dirname, "./src/components"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@utils": path.resolve(__dirname, "./src/utils"),
       "@types": path.resolve(__dirname, "./src/types"),
       "@store": path.resolve(__dirname, "./src/store"),
+      "@fb": path.resolve(__dirname, "./src/firebase"),
     },
   },
   plugins: [react(), tailwindcss()],
